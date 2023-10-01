a9d866b6f006c39c87e203ece5f9342292128314
Sergei Antipov <mrantip90@gmail.com>
2023-10-01 21:04:56 +0300

    v3 поправлены настройки размеров окна браузера

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,8 +5,8 @@ from selene import browser
 @pytest.fixture(scope="function")
 def set_demoga():
     browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_height = 800
-    browser.config.window_width = 400
+    browser.config.window_height = 854
+    browser.config.window_width = 480
     browser.config.timeout = 4.0
 
     yield
